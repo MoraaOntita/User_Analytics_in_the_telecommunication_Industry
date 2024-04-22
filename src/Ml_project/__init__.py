5066b1854d27997271cb51067eca5abdb2d90ba3
MoraaOntita <moraaontita1844@gmail.com>
2024-04-22 07:37:12 +0300

    Updating the project Structure

---
+++ src/Ml_project/__init__.py
@@ -0,0 +1,5 @@
+
+__author__ = "Moraa Ontita"
+__author_email__ = "moraaontita9@gmail.com"
+__repository__ = "https://github.com/MoraaOntita/User_Analytics_in_the_telecommunication_Industry.git"
+__version__ = "1.0.0"
